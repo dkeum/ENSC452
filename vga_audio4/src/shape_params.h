0cf4fa904e3131ec4a63959544941140feec52c1
Daniel Keum <dkeum@sfu.ca>
2023-03-04 18:59:26 -0800

    working code, need updates to the DMA

---
+++ vga_audio4/src/shape_params.h
@@ -0,0 +1,137 @@
+#ifndef __Shapes__
+#define __Shapes__
+
+#include "xuartps.h"
+#include "xil_cache.h"
+#include "sleep.h"
+
+#define BUTTON_CHANNEL 1
+#define SWITCH_CHANNEL 2
+#define UART_BASEADDR XPAR_PS7_UART_1_BASEADDR
+
+#define is_light_pattern_changed (*(volatile unsigned long *)(0xFFFF0000)) //shared memory space for if light pattern mode is changed
+#define light_pattern_mode (*(volatile unsigned long *)(0xFFFF0010)) // light pattern mode
+#define audio_volume_change (*(volatile unsigned long *)(0xFFFF0020))
+#define tempo_speed_change (*(volatile unsigned long *)(0xFFFF0030))
+
+// streaming cannot speed up music
+//0, recording we can increase or decrease tempo
+//1 streaming we cannot increase tempo (but can decrease the tempo)
+//2 music is stopped and so we stop the shapes from moving
+//3 music is playing back we can increase or decrease tempo
+#define is_music_streaming (*(volatile unsigned long *)(0xFFFF0040))
+#define sound_effect_added (*(volatile unsigned long *)(0xFFFF0050))
+#define reset_bin_counters (*(volatile unsigned long *)(0xFFFF0060))
+#define prng_output        (int*)(0xFFFF0070) // will hold at most 20 int values
+
+
+
+
+
+// Base class
+class Shape {
+   public:
+	  void draw_line(int x1,int x2,int y1,int y2, int color);// http://www.brackeen.com/vga/source/djgpp20/lines.c.html
+	  void fill_method(int x1,int y1,int length, int color, int background_color);
+
+   protected:
+      int * image_buffer_pointer = (int *)0x00900000; // The address of the image buffer pointer will be here
+};
+
+// Derived class
+class Square: public Shape {
+   public:
+	void draw_Square(int x1,int x2, int y1,int y2, int color);
+	void draw_Square(int x1,int y1, int size, int color);
+	void draw_Square_rotation(int x1,int y1, int size, int color, double rotation);
+	int draw_Square_flexible_version(int x1,int y1, int size, int color, double rotation, int background_color, int elongate_on);
+	void draw_Square_v2(int x1,int y1, int size, int color);
+	void draw_Square_flexible_version_v2(int x1,int y1, int size, int color, double rotation, int background_color);
+
+};
+
+// Derived class
+class Triangle: public Shape {
+   public:
+	void draw_Triangle(int x1, int y1,int color);
+	void draw_Triangle(int x1, int y1,int size,int color);
+	void draw_Triangle(int x1, int y1,int size,int color, double rotation);
+	int draw_Triangle_flexible_version(int x1, int y1,int size,int color, double rotation,int background_color, int elongate_on);
+	//void draw_line(int x1,int x2,int y1,int y2, int color); // http://www.brackeen.com/vga/source/djgpp20/lines.c.html
+
+};
+
+// Derived class
+
+// resources used here: http://www.brackeen.com/vga/source/djgpp20/circle.c.html
+class Circle: public Shape {
+   public:
+//	void draw_Circle(int x1, int y1,int radius,int color);
+	void draw_Circle(int x1, int y1,int size,int color);
+	int draw_Circle_flexible_version(int x1, int y1,int size,int color,int background_color, int elongate_on);
+
+};
+
+
+// Derived class
+class Pentagon: public Shape {
+   public:
+	void draw_Pentagon(int x1, int y1,int color);
+	void draw_Pentagon(int x1, int y1,int size,int color);
+	void draw_Pentagon(int x1, int y1,int size,int color,double rotation);
+	int draw_Pentagon_flexible_version(int x1, int y1,int size,int color, double rotation,int background_color, int elongate_on);
+
+	// void draw_line(int x1,int x2,int y1,int y2, int color); // http://www.brackeen.com/vga/source/djgpp20/lines.c.html
+};
+
+
+// Derived class
+
+/*
+1280x1024 screen (widthxheight)
+Size of the bin
+1) Width 1280-200 = 1080 offset from having the sides clear
+2) there will be 5 bins and there will be space in between; 5x+4y = 1080, suppose that the spacing is 50 (y=50) then x=166
+3) each bin will have a width of 166.
+4) for the bin height, 100
+5) The overall structure of the bin should be square with a semi-circle on top
+6) each bin should label the frequency range (may need to adjust the width formula to this)
+7)
+
+*/
+class Bins{
+   public:
+	void draw_Bin(int x, int y1,int y2, int frequency_range, int color);
+	int * image_buffer_pointer = (int *)0x00900000;
+	int counter=0;
+};
+
+
+
+
+/////////////////////-------------------------------------------------------------------------------//////////////////////
+
+// Derived class
+class Sound_effects_name: public Shape {
+   public:
+	void draw_sound_effects_name(int x, int y);
+	void display_light_pattern_mode(int mode_num);
+
+
+	int * image_buffer_pointer = (int *)0x00900000;
+};
+
+
+// Derived class
+class Background: public Shape {
+   public:
+	int set_background(int background_mode);
+
+    int NUM_BYTES_BUFFER = 5242880;
+	int * image_buffer_pointer = (int *)0x00900000;
+};
+
+
+
+
+#endif
---
+++ vga_audio_test4/src/adventures_with_ip.h
@@ -0,0 +1,107 @@
+/*
+ * adventures_with_ip.h
+ *
+ * Main header file.
+ */
+
+#ifndef ADVENTURES_WITH_IP_H_
+#define ADVENTURES_WITH_IP_H_
+
+/* ---------------------------------------------------------------------------- *
+ * 								Header Files									*
+ * ---------------------------------------------------------------------------- */
+#include <stdio.h>
+#include <xil_io.h>
+#include <sleep.h>
+#include "xiicps.h"
+#include <xil_printf.h>
+#include <xparameters.h>
+#include "xgpio.h"
+#include "xuartps.h"
+#include "stdlib.h"
+#include <math.h>
+#include "xaxidma.h"
+
+/* ---------------------------------------------------------------------------- *
+ * 							Custom IP Header Files								*
+ * ---------------------------------------------------------------------------- */
+#include "audio.h"
+//#include "lms_pcore_addr.h"
+//#include "xnco.h"
+
+/* ---------------------------------------------------------------------------- *
+ * 							Prototype Functions									*
+ * ---------------------------------------------------------------------------- */
+void menu();
+void tonal_noise();
+void audio_stream();
+void audio_record();
+void audio_play();
+void audio_speedup();
+void audio_slowdown();
+void audio_raiseVolume();
+void audio_lowerVolume();
+void audio_normal_speed();
+void sine_wave();
+void lms_filter();
+unsigned char gpio_init();
+void nco_init(void *InstancePtr);
+void generate_PRNG(XAxiDma myDma);
+
+/* ---------------------------------------------------------------------------- *
+ * 						Redefinitions from xparameters.h 						*
+ * ---------------------------------------------------------------------------- */
+
+#define UART_BASEADDR XPAR_PS7_UART_1_BASEADDR
+
+#define BUTTON_SWITCH_BASE XPAR_GPIO_1_BASEADDR
+//#define LED_BASE XPAR_LED_CONTROLLER_0_S00_AXI_BASEADDR
+#define BUTTON_SWITCH_ID XPAR_GPIO_1_DEVICE_ID
+#define DDR_MEM_ADDR XPAR_PS7_DDR_0_S_AXI_BASEADDR
+#define L2_Cache XPAR_PS7_L2CACHEC_0_S_AXI_BASEADDR
+
+/* ---------------------------------------------------------------------------- *
+ * 							Define GPIO Channels								*
+ * ---------------------------------------------------------------------------- */
+#define BUTTON_CHANNEL 1
+#define SWITCH_CHANNEL 2
+
+/* ---------------------------------------------------------------------------- *
+ * 							Audio Scaling Factor								*
+ * ---------------------------------------------------------------------------- */
+#define SCALE 7
+
+/* ---------------------------------------------------------------------------- *
+ * 							Global Variables									*
+ * ---------------------------------------------------------------------------- */
+//XIicPs Iic;
+//XGpio Gpio; // Gpio instance for buttons and switches
+//XNco Nco;
+
+/* ---------------------------------------------------------------------------- *
+ * 							Shared Memory variables controlled via interrupts									*
+ * ---------------------------------------------------------------------------- */
+
+#define is_light_pattern_changed (*(volatile unsigned long *)(0xFFFF0000)) //shared memory space
+#define light_pattern_mode (*(volatile unsigned long *)(0xFFFF0010)) // light pattern mode
+#define audio_volume_change (*(volatile unsigned long *)(0xFFFF0020))
+#define tempo_speed_change (*(volatile unsigned long *)(0xFFFF0030))
+
+// streaming cannot speed up music
+//0, recording we can increase or decrease tempo
+//1 streaming we cannot increase tempo (but can decrease the tempo)
+//2 music is stopped and so we stop the shapes from moving
+//3 music is playing back we can increase or decrease tempo
+#define is_music_streaming (*(volatile unsigned long *)(0xFFFF0040))
+#define sound_effect_added (*(volatile unsigned long *)(0xFFFF0050))
+#define reset_bin_counters (*(volatile unsigned long *)(0xFFFF0060))
+#define prng_output        (int*)(0xFFFF0070) // will hold at most 20 int values
+
+
+// state machine
+// when audio record ends with 10 seconds then it will go back to streaming immediately
+
+
+#endif /* ADVENTURES_WITH_IP_H_ */
+
+
